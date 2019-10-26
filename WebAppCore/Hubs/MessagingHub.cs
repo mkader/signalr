e181c9247e4799d0e21bb94530a7743a2b5fee0a
mohideen.kader <mohideen.kader@TXGDC-KMOHIDEEN>
2019-10-25 20:51:19 -0500

    Add project files.

---
+++ WebAppCore/Hubs/MessagingHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCore.Hubs
+{
+    // 1. Add ASP.NET Core SignalR
+    // 2. Creating a SignalR Hub
+    public class MessagingHub : Hub
+    {
+        //empty hub class
+      
+        public Task NotifyConnection()
+        {
+            return Clients.All.SendAsync("TestBrodcasting",
+            $"Testing a Basic HUB at {DateTime.Now.ToLocalTime()}");
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ WebAppCore/wwwroot/js/app.js
@@ -0,0 +1,65 @@
+﻿"use strict";
+
+//1
+let connection = new signalR.HubConnectionBuilder()
+    .withUrl("/messagingHub")
+    .build();
+
+//2
+//function getConnected() {
+document.getElementById("btnConnnect").addEventListener("click", function (event) {
+    if (connection.connection.connectionState==2) {
+        connection.start().then(function () {
+            document.getElementById('connectAlertDiv').style.display = "block";
+            //document.getElementById('btnConnnect').style.display = "none";
+            TestConnection();
+            
+        }).catch(function (err) {
+            return console.error(err.toString());
+        });
+    } else {
+        TestConnection();
+    }
+    event.preventDefault();
+});
+
+//3
+function TestConnection() {
+    connection.invoke("NotifyConnection").catch(function (err) {
+        return console.error(err.toString());
+    });
+}
+
+//4
+connection.on("TestBrodcasting", function (time) {
+    document.getElementById('broadcastDiv').innerHTML = time;
+    document.getElementById('broadcastDiv').style.display = "block";
+});
+
+
+//chat code
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
